3d2a4e0b539ceea4f259ca4503b336f872135a3b
luckyfrappe <212164038+luckyfrappe@users.noreply.github.com>
2026-01-17 10:52:08 +0100

    Start home app

--- stocka_marketplace/urls.py
+++ stocka_marketplace/urls.py
@@ -5,4 +5,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('home.urls')),
+    path('accounts/', include('allauth.urls')),
+    # path('products/', include('products.urls')),
+    # path('bag/', include('bag.urls')),
+    # path('checkout/', include('checkout.urls')),
+    # path('profiles/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
