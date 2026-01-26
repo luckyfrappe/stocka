c70eaff138cd8fdc3b3607211e6a95b7dab1aae2
luckyfrappe <212164038+luckyfrappe@users.noreply.github.com>
2026-01-26 11:47:37 +0100

    Refactor account templates for improved styling and structure

--- stocka_marketplace/settings.py
+++ stocka_marketplace/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
 
     # other
     'crispy_forms',
+    'crispy_bootstrap5',
     #'storages',
 ]
 
@@ -72,7 +73,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'stocka_marketplace.urls'
 
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 TEMPLATES = [
     {
