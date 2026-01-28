4ee0cbc065f9b013b960f5c9e6d00e04d51e8e53
luckyfrappe <212164038+luckyfrappe@users.noreply.github.com>
2026-01-28 22:42:48 +0100

    Fully integrate profiles

--- profiles/forms.py
+++ profiles/forms.py
@@ -25,11 +25,15 @@ class UserProfileForm(forms.ModelForm):
 
         self.fields['default_phone_number'].widget.attrs['autofocus'] = True
         for field in self.fields:
-            if field != 'default_country':
+            if field != 'default_country' and field != 'marketing_opt_in':
                 if self.fields[field].required:
                     placeholder = f'{placeholders[field]} *'
                 else:
                     placeholder = placeholders[field]
                 self.fields[field].widget.attrs['placeholder'] = placeholder
-            self.fields[field].widget.attrs['class'] = 'border-black rounded-0 profile-form-input'
-            self.fields[field].label = False
+            self.fields[field].widget.attrs['class'] = 'border-black profile-form-input'
+            
+            if field != 'marketing_opt_in':
+                self.fields[field].label = False
+            else:
+                self.fields[field].label = 'Subscribe to our newsletter'
